23639aa78900e4766f3df0e991917b720687f633
MohaimanulIslamHasib <64539614+MohaimanulIslamHasib@users.noreply.github.com>
2020-07-02 21:21:21 +0600

    Ajax

--- ajax.php
+++ ajax.php
@@ -7,25 +7,25 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
-    <form onsubmit="return false" action="" method="POST">
+    <form onsubmit="" action="" method="POST">
     <input type="text" name="usrName" id="name">
     <input type="text" name="usrPwd" id="pass">
     <input type="submit" name="subButton" id="SB">
      </form>
-   <div id=spin></div>
+   <div id='spin'></div>
    <script>
-       $(document).ready(function(){
+      
         $('#SB').click(function () {
     var myUsr=$('#name').val()
     var myPwd=$('#pass').val()
     $('#spin').html('<img src='images/spinner.gif' alt=''/>)
     $.post('sendAjax.php',{uName:myUsr,uPwd:myPwd},function (mydata) {
-        
+        alert(mydata)
     })
 })
 
 
-       })
+   
 
 
    </script>
